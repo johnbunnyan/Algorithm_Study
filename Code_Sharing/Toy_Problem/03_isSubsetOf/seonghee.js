d4604500d290549491e177c97e1cfe3c4196b195
Kimseonghee <kimsunghee625@gmail.com>
2021-07-30 17:32:42 +0900

    toy_03_isSubsetOf(김성희)

--- Code_Sharing/Toy_Problem/03_isSubsetOf/seonghee.js
+++ Code_Sharing/Toy_Problem/03_isSubsetOf/seonghee.js
@@ -1,7 +1,7 @@
 const isSubsetOf = function (base, sample) {
   base.sort((a, b) => a - b);
   sample.sort((a, b) => a - b);
-
+  //오름 차순 정렬
   let idx = 0;
 
   for (let i = 0; i < sample.length; i++) {
